e7870c3a6aa542797981ebaf91ee82c5a8800e50
Dan Herzig <Dan.Herzig@ellucian.com>
2018-06-19 11:59:13 -0400

    Basic implementation

--- public/scripts/SpeechRecognition.js
+++ public/scripts/SpeechRecognition.js
@@ -1,5 +1,4 @@
-(function () {
-
+$(document).ready(function() {
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
     const recognition = new SpeechRecognition();
@@ -8,7 +7,7 @@
 
     recognition.addEventListener('result', e => {
         console.log(e.results);
-        
+
         // const transcript = Array.from(e.results)
         //     .map(result => result[0])
         //     .map(result => result.transcript)
@@ -22,4 +21,4 @@
     });
 
     recognition.start();
-});
+})
